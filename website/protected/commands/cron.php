94a096c57ad91644ad3986b6d049fbbf471cc57b
Shihao <subchap10@gmail.com>
2013-10-07 12:02:49 -0400

    cron

--- website/protected/commands/cron.php
+++ website/protected/commands/cron.php
@@ -3,7 +3,8 @@
 defined('YII_DEBUG') or define('YII_DEBUG',true);
  
 // including Yii
-require_once(dirname(__FILE__).'/../../../../../yii/framework/yii.php');
+// require_once(dirname(__FILE__).'/../../../../../yii/framework/yii.php');
+require_once('/yii/framework/yii.php');
 
 // we'll use a separate config file
 $configFile=dirname(__FILE__).'/../config/console.php';
