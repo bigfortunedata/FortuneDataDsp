93221f2c39582977d96be73fd357a3bd5e531520
tonyzhy <tony_zhy@hotmail.com>
2013-10-24 21:06:14 -0700

    added 'Action Now' link and chinese ads

--- website/themes/bootstrap/views/site/index.php
+++ website/themes/bootstrap/views/site/index.php
@@ -22,6 +22,8 @@ $this->pageTitle=Yii::app()->name;
   ),
 ));?>
 </br> 
+<p style=" font-size:22px;" align="center">
+       <b>我们利用实时竞价和受众定位技术，在高端媒体把您的广告精准地投送到全球任何一个地区的客户</b>    </p>
 <p style=" font-size:20px;" align="center">
      <i> Chinese travelers spent a record <b>US$102</b> billion on international tourism in 2012, a <b>40%</b> rise from US$73 billion in 2011. </i>  </p>
   <p style=" font-size:20px;" align="center">   <i> Overseas Chinese consumers market continues to grow rapidly and accounts for an increasing share of global sales for certain sectors. </i>  </p>
@@ -35,7 +37,7 @@ $this->pageTitle=Yii::app()->name;
 <p>North American and Europe businesses have largely missed the opportunity to engage with this emerging consumer group because the available media buying tools lack the capabilities of running campaigns targeting the group meaningfully and cost-effectively.</p>
 <td width=" 50%">
 <p style=" font-size:20px;"> <i> <b>Our Solution</b></i></p>
-<p>Fortune Data programmatic advertising platform provides businesses one-stop solution to engage Chinese consumers in real time and deliver your relevant marketing campaign content across all vast networks. </p>
+<p>Fortune Data programmatic advertising platform provides businesses one-stop solution to engage Chinese consumers in real time and deliver your relevant marketing campaign content across all vast networks. <a href="http://dsp.bigfortunedata.com/registration/registration/registration">Action Now</a> </p>
 
 </td>
 </tr>
