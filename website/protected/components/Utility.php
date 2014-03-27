e9dc60b61056afd160c04fcabad44a66e0587cd1
tonyzhy <tony_zhy@hotmail.com>
2014-03-26 20:47:31 -0700

    fix BC sales tax bug

--- website/protected/components/Utility.php
+++ website/protected/components/Utility.php
@@ -48,7 +48,7 @@ class Utility {
         $tax_rate = 0;
 
         if (($country == 'CANADA') and ($state == 'BC')) {
-            $tax_rate = 0.5;
+            $tax_rate = 0.05;
         } elseif (($country == 'CANADA') and ($state == 'ON')) {
             $tax_rate = 0.13;
         } elseif (($country == 'CANADA') and ($state == 'PE')) {
